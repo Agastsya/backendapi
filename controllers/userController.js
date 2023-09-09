f405c50b7e9c4f29c75ff79516e7c1d806140b65
agastsya <agastsya007@gmail.com>
2023-09-09 18:38:22 +0530

    Production mode completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,6 +61,8 @@ export const logout = async (req, res) => {
     res
       .cookie("token", "", {
         expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? "false" : true,
       })
       .json({
         success: "true",
